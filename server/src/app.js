d32f7bd78a631df3dcb6b537c956bd15ed1ffdf4
Nishtha Vatsa <nishthavatsa@gmail.com>
2026-06-25 12:46:21 +0530

    Configure CORS for deployed frontend

--- server/src/app.js
+++ server/src/app.js
@@ -10,11 +10,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(cookieParser());
 
